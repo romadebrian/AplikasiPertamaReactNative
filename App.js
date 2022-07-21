77b7585a3be4b67044087fb0d31167d0a04962fb
romadebrian <roma_coll04@yahoo.com>
2022-07-21 20:30:34 +0700

    Styling Component

--- App.js
+++ App.js
@@ -1,101 +1,133 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Text, TextInput, View, Image, StyleSheet} from 'react-native';
+import Miku from './Miku.jpg';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  return <StylingReactNativeComponent />;
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const StylingReactNativeComponent = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={styles.text}>Styling Componenet</Text>
+      <View
+        Style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#0abde3',
+          borderWidth: 2,
+          borderColor: '#5f27cd',
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+      <View
+        style={{
+          padding: 12,
+          backgroundColor: '#F2F2F2',
+          width: 212,
+          borderRadius: 8,
+        }}>
+        <Image
+          source={Miku}
+          style={{width: 188, height: 107, borderRadius: 8}}
+        />
+        <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
+          Gambar Hatsune Miku
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#F2994A',
+            marginTop: 12,
+          }}>
+          Rp. 25.000.000
+        </Text>
+        <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
+          Jakarta Barat
+        </Text>
+        <View
+          style={{
+            backgroundColor: '#6FCF97',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Beli
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#10ac84',
+    marginLeft: 20,
+    marginTop: 40,
+  },
+});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const SampleComponent = () => {
+  <View>
+    <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}}></View>
+    <Text>Prawito</Text>
+    <Hudoro />
+    <Text>Mila</Text>
+    <Text>Azmy</Text>
+    <Photo />
+    <TextInput style={{borderWidth: 1}} />
+    <BoxGreen />
+    <Profile />
+  </View>;
+};
 
+const Hudoro = () => {
+  return <Text>Hudoro Mangku Negoro</Text>;
+};
+
+const Photo = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <View style={styles.body}>
-            <Text>Hello Wold ...</Text>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Image
+      source={{
+        uri: 'https://static.tvtropes.org/pmwiki/pub/images/hololive_fantasy_by_shiranui_flare_6.png',
+      }}
+      style={{width: 100, height: 100}}
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini component dari class</Text>;
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{
+            uri: 'https://rare-gallery.com/thumbnail/1347420-Shiranui-Flare-HololiveVirtual-Youtuber-HD-Wallpaper.png',
+          }}
+          style={{width: 100, height: 100, borderRadius: 50}}
+        />
+        <Text style={{color: 'blue', fontSize: 24}}>Ini Vtuber</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
