5310c2452cc10f2eeb7ff3991b084f9ae85e1e0e
romadebrian <roma_coll04@yahoo.com>
2022-07-23 20:36:06 +0700

    Memahami siklus hidup lifecycle Hooks Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, ScrollView, Text} from 'react-native';
 import SampleComponent from './pages/SampleComponent';
 import StylingReactNativeComponent from './pages/StylingComponent';
 import MateriFlexBox from './pages/FlexBox';
 import PositionReactNative from './pages/Position';
 const App = () => {
+  const [isShow, setIsShow] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShow(false);
+    }, 6000);
+  });
   return (
     <View>
       <ScrollView>
-        <SampleComponent />
-        <StylingReactNativeComponent />
-        <MateriFlexBox />
-        <PositionReactNative />
+        {/* <SampleComponent />
+        <StylingReactNativeComponent /> */}
+        {isShow && <MateriFlexBox />}
+        {/* <PositionReactNative /> */}
       </ScrollView>
-      <Text>Test</Text>
     </View>
   );
 };
--- src/pages/FlexBox/index.js
+++ src/pages/FlexBox/index.js
@@ -1,48 +1,130 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {Image, Text, View} from 'react-native';
 
-class FlexBox extends Component {
-  render() {
-    return (
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: 'gray',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}>
-          <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
-          <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
-          <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
-          <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-          <Text>Beranda</Text>
-          <Text>Video</Text>
-          <Text>Playlist</Text>
-          <Text>Komunitas</Text>
-          <Text>Channel</Text>
-          <Text>Tentang</Text>
-        </View>
-        <View
-          style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
-          <Image
-            source={{
-              uri: 'https://rare-gallery.com/thumbnail/1347420-Shiranui-Flare-HololiveVirtual-Youtuber-HD-Wallpaper.png',
-            }}
-            style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
-          />
-          <View>
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>
-              Shiranui Flare
-            </Text>
-            <Text>777 k subscriber</Text>
-          </View>
+// class FlexBox extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log('===> construktor');
+//     this.state = {
+//       subscriber: 200,
+//     };
+//   }
+
+//   componentDidMount() {
+//     console.log('===> component did mount');
+//     setTimeout(() => {
+//       this.setState({subscriber: 777});
+//     }, 2000);
+//   }
+
+//   componentDidUpdate() {
+//     console.log('component did update');
+//   }
+
+//   componentWillUnmount() {
+//     console.log('===> component will unmount');
+//   }
+
+//   render() {
+//     console.log('===> render');
+//     return (
+//       <View>
+//         <View
+//           style={{
+//             flexDirection: 'row',
+//             backgroundColor: 'gray',
+//             alignItems: 'center',
+//             justifyContent: 'space-between',
+//           }}>
+//           <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
+//           <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
+//           <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
+//           <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
+//         </View>
+//         <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+//           <Text>Beranda</Text>
+//           <Text>Video</Text>
+//           <Text>Playlist</Text>
+//           <Text>Komunitas</Text>
+//           <Text>Channel</Text>
+//           <Text>Tentang</Text>
+//         </View>
+//         <View
+//           style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+//           <Image
+//             source={{
+//               uri: 'https://rare-gallery.com/thumbnail/1347420-Shiranui-Flare-HololiveVirtual-Youtuber-HD-Wallpaper.png',
+//             }}
+//             style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
+//           />
+//           <View>
+//             <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+//               Shiranui Flare
+//             </Text>
+//             <Text>{this.state.subscriber} k subscriber</Text>
+//           </View>
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+const FlexBox = () => {
+  const [subscriber, setSubscriber] = useState(200);
+
+  useEffect(() => {
+    console.log('===> component did mount');
+    setTimeout(() => {
+      setSubscriber(777);
+    }, 2000);
+
+    return () => {
+      console.log('did update');
+    };
+  }, [subscriber]);
+
+  // useEffect(() => {
+  //   console.log('===> component did update');
+  //   setTimeout(() => {
+  //     setSubscriber(777);
+  //   }, 2000);
+  // }, [subscriber]);
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: 'gray',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
+        <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
+        <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
+        <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
+      </View>
+      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <Text>Beranda</Text>
+        <Text>Video</Text>
+        <Text>Playlist</Text>
+        <Text>Komunitas</Text>
+        <Text>Channel</Text>
+        <Text>Tentang</Text>
+      </View>
+      <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+        <Image
+          source={{
+            uri: 'https://rare-gallery.com/thumbnail/1347420-Shiranui-Flare-HololiveVirtual-Youtuber-HD-Wallpaper.png',
+          }}
+          style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
+        />
+        <View>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Shiranui Flare</Text>
+          <Text>{subscriber} k subscriber</Text>
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 export default FlexBox;
